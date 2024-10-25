b40e56230f78922e188a0f80bcd6d50bc707b581
jxron-pixel <jjspyderx@gmail.com>
2024-10-25 17:30:29 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+name = "Jaron"
+age = 11
+student = True
+weight = 37.4 
+
+print("\n after typcasting. . .")
+age = str(age)
+print(age)
+print("data type of age is ",type(age))
+weight = int(weight)
+print(weight)
+print("type of data is ",type(weight))
+student = str(student)
+print(student)
+print("data type of student is ",type(student))
+
